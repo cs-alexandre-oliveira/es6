f56346e7d1023256d03e9ed1fd9dd7726db60c59
Alexandre Oliveira <alexandre.oliveira@concrete.com.br>
2018-04-26 13:47:12 -0300

    privando acesso à classe

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,12 +1,24 @@
 class Negociacao {
+  /* Quando utilizado o Underline a propriedade fica exclusiva para acesso da Classe */
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+  /* Quando uma Function é chamada dentro de uma Classe ela é chamada de Método*/
+  getVolume() {
+    return this._quantidade * this._valor;
+  }
 
-    constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
-    }
-/* Quando uma Function é chamada dentro de uma Classe ela é chamada de Método*/
-    obtemVolume() {
-        return this.quantidade * this.valor;
-    }
+  getData() {
+    return this._data;
+  }
+
+  getQuantidade() {
+    return this._quantidade;
+  }
+
+  getValor() {
+    return this._valor;
+  }
 }
