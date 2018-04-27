4cd82aa64f13ac6683fd4f78ceff5a180418c58b
Alexandre Oliveira <alexandre.oliveira@concrete.com.br>
2018-04-27 15:42:13 -0300

    Ajustando os metodos da classe NegociacaoController e ajustes do campo data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,9 +1,9 @@
 class NegociacaoController {
 
     constructor() {
-/*bind é utilizado para continuar a relação do documento com o Método querySelector */
+//bind é utilizado para continuar a relação do documento com o Método querySelector
         let $ = document.querySelector.bind(document);
-        
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
@@ -12,9 +12,24 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+
+//console.log(typeof(this._inputData.value)); 
+        let data = new Date(
+//spread operator (...) é usado para apontar que o primeiro elemento do array é o primeiro parametro passado para a function    
+            ...this._inputData.value
+                .split('-')
+//quando usar arrow function e tiver somente uma linha não necessita de (function, {} e return)
+                .map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
     }
 }
+
