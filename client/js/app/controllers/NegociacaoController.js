0741c8b9e284d5b4bd91d8abd7bd1ca240ee5885
Alexandre DEV <calexandredev@gmail.com>
2018-04-28 23:00:06 -0300

    Blindando a criação de negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,19 +7,31 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao);
+        this._limpaFormulario();
+    }
+    //metodo privado na Classe é criado com o underline _nomeMetodo
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputValor.value = 0.0;
+        this._inputQuantidade.value = 1;
+
+        this._inputData.focus();
     }
 }
 
--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,13 +6,16 @@ class DateHelper {
 
 // static é um método que pertencem a definição da classe evitando criar instancias
   static textoParaData(texto) {
+    //Expressão Regular para validar o formato de data
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+      throw new Error('Formato deve estar diferente de YYYY/MM/DD!');
+
     //spread operator (...) é usado para apontar que o primeiro elemento do array é o primeiro parametro passado para a function
     return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); //quando usar arrow function e tiver somente uma linha não necessita de (function, {} e return)
   }
 
   static dataParaTexto(data) {
-    return data.getDate()
-      + '/' + (data.getMonth() + 1)
-      + '/' + data.getFullYear();
+    // utilizando template String
+    return `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`;
   }
 }
