4b607b2ba7fc859a636e26af9f5254173d771069
Alexandre DEV <calexandredev@gmail.com>
2018-04-30 11:55:48 -0300

    identando e comentando partes do codigo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,9 +1,8 @@
 class NegociacaoController {
-
+    
     constructor() {
-        //bind é utilizado para continuar a relação do documento com o Método querySelector
+       //bind é utilizado para continuar a relação do documento com o Método querySelector 
         let $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
@@ -12,29 +11,28 @@ class NegociacaoController {
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
     }
-
+    
     adiciona(event) {
+        
         event.preventDefault();
-
-        this._listaNegociacoes.adiciona(this._criaNegociacao);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
-        this._limpaFormulario();
+        this._limpaFormulario();   
     }
     //metodo privado na Classe é criado com o underline _nomeMetodo
     _criaNegociacao() {
+        
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);    
     }
-
+    
     _limpaFormulario() {
+     
         this._inputData.value = '';
-        this._inputValor.value = 0.0;
         this._inputQuantidade.value = 1;
-
-        this._inputData.focus();
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
-
