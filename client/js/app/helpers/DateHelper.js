fe5e51e1236e6116c6e92e62e60badfaff525a92
Alexandre Oliveira <alexandre.oliveira@concrete.com.br>
2018-05-18 09:35:14 -0300

    removendo o inputDate para tornar o codigo compativel com o Firefox

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -12,10 +12,10 @@ class DateHelper {
     
     static textoParaData(texto) {
         // static é um método que pertencem a definição da classe evitando criar instancias
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto)) 
         //Expressão Regular para validar o formato de data
-            throw new Error('Deve estar no formato aaaa-mm-dd');
+            throw new Error('Deve estar no formato dd/mm/aaaa');
         //spread operator (...) é usado para apontar que o primeiro elemento do array é o primeiro parametro passado para a function     
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); //quando usar arrow function e tiver somente uma linha não necessita de (function, {} e return)
+        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2)); //quando usar arrow function e tiver somente uma linha não necessita de (function, {} e return)
     }
 }
